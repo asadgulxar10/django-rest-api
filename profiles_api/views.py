cb3963225c3311003f63847c0799db0da475440b
Asad <asadgulxar10@gmail.com>
2021-02-04 14:45:04 +0500

    Added ViewSet

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from profiles_api import serializers
+from rest_framework import viewsets
 
 class HellowApiView(APIView):
     """Test Api view"""
+
+    serializer_class= serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -16,3 +20,79 @@ class HellowApiView(APIView):
         ]
 
         return Response({'message':'AOA', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        # return Response(r)
+        serializer =self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name =serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial update of object"""
+
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+
+        return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    
+    serializer_class= serializers.HelloSerializer
+
+    def list(self, request):
+
+        a_viewset=[
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Authomatically maps to URLs using Routes',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello', 'a_viewset':a_viewset})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name');
+            message= f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+
+        return Response({'http_method': 'DELETE'})
